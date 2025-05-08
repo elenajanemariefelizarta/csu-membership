e4d304157f07c5734b6c3d8407bd2d5d4f2d2cf9
unknown <elenafelizarta@gmail.com>
2025-05-08 11:38:05 +0800

    Fix route for Login page

--- CsuMembership/src/router/index.js
+++ CsuMembership/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
+      path: '/Login',
       name: 'Login',
       component: LoginViews
     },
