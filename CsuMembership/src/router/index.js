08a7f1c304e7d8b65f7185c56bd8618be82704c0
unknown <elenafelizarta@gmail.com>
2025-05-08 11:42:07 +0800

    feat: added a path login in index.js

--- CsuMembership/src/router/index.js
+++ CsuMembership/src/router/index.js
@@ -6,8 +6,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/Login',
-      name: 'Login',
+      path: '/login',
+      name: 'login',
       component: LoginViews
     },
 
